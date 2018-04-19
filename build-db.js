beb9d8958495bf5ada11cf33dd5df12ee288fc1e
BrandonAllenGreen <green04@gmail.com>
2018-04-19 11:06:55 -0500

    changed order of incoming objs in build-db

--- build-db.js
+++ build-db.js
@@ -1,8 +1,8 @@
 'use strict';
 
 let models = require('./models');
-let { products } = require('./seeders/products');
 let { productTypes } = require('./seeders/product-types');
+let { products } = require('./seeders/products');
 
 models.sequelize
   .sync({ force: true })
