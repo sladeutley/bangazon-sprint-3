f9a272922576cd49d80a9bc9d1312a7b67e9a1eb
Aaron Wolfcale <ajwolfcale@gmail.com>
2018-04-19 09:46:02 -0500

    paymentType Data

--- generators/payment-type-faker.js
+++ generators/payment-type-faker.js
@@ -6,14 +6,11 @@ module.exports.generatePaymentTypes = () => {
   let paymentTypes = [];
   let paymentTypeArr = ["Visa", "Amex", "Master Card"];
   let payNum;
-
   for (let i = 0; i < 10; i++) {
     payNum = Math.floor(Math.random() * paymentTypeArr.length);
     let name =  paymentTypeArr[payNum];
     let acct_num = faker.finance.account();
     let user_id = faker.random.number({ min: 1, max: 5 });
-    console.log(payNum);
-
     paymentTypes.push({
       name,
       acct_num,
