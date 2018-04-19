50f18b30d77f92084d7d39812b123915abac9f86
BrandonAllenGreen <green04@gmail.com>
2018-04-19 11:18:42 -0500

    prettier formatted paymenttype file

--- models/paymenttype.js
+++ models/paymenttype.js
@@ -1,16 +1,20 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
-  var PaymentType = sequelize.define('PaymentType', {
-    name: DataTypes.STRING,
-    acct_num: DataTypes.INTEGER
-  }, {tableName: "payment-types", timestamps: false});
+  var PaymentType = sequelize.define(
+    'PaymentType',
+    {
+      name: DataTypes.STRING,
+      acct_num: DataTypes.INTEGER
+    },
+    { tableName: 'payment-types', timestamps: false }
+  );
   PaymentType.associate = function(models) {
     PaymentType.belongsTo(models.User, {
-      foreignKey: "userId"
+      foreignKey: 'userId'
     });
     PaymentType.belongsTo(models.Order, {
-      foreignKey: "paymentTypeId"
+      foreignKey: 'paymentTypeId'
     });
-  }
+  };
   return PaymentType;
 };
