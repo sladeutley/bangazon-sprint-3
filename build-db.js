f719536896d20fa4f50c02ad03417b8f7bbac0c4
gmarkay <gmarkay@gmail.com>
2018-04-19 09:33:44 -0500

    getting stuff

--- build-db.js
+++ build-db.js
@@ -3,7 +3,7 @@
 let models = require("./models");
 
 models.sequelize.sync({ force: true })
-// Write bulkCreate functions here
-.then( () => {
-  process.exit();
-});
+  // Write bulkCreate functions here
+  .then(() => {
+    process.exit();
+  });
