58ae8a134f57dcd60af293159a1dd0613ce53a3b
Valerie Freeman <valerah7@gmail.com>
2018-04-20 10:43:11 -0500

    create function to get products by seller id

--- controllers/productCtrl.js
+++ controllers/productCtrl.js
@@ -2,7 +2,6 @@
 
 module.exports.getProductTypes = (req, res, next) => {
   const { ProductType } = req.app.get('models');
-
   ProductType.findAll()
     .then(productTypes => {
       res.status(200).json(productTypes);
@@ -11,4 +10,21 @@ module.exports.getProductTypes = (req, res, next) => {
       console.log('Something went wrong!', err);
       res.status(500).json({ error: err });
     });
+};
+
+module.exports.getProductsByUserId = (req, res, next) => {
+  const { Product } = req.app.get('models');
+  console.log('req', req);
+  Product.findAll({
+    where: {
+      userId: req.user.id
+    }
+  })
+  .then(products => {
+    res.status(200).json(products);
+  })
+  .catch(err => {
+    console.log('Something went wrong', err);
+    res.status(500).json({ error: err});
+  });
 };
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,8 +3,9 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getProductTypes } = require('../controllers/productCtrl.js');
+const { getProductTypes, getProductsByUserId } = require('../controllers/productCtrl.js');
 
 router.get('/prodTypes', getProductTypes);
+router.get('/myProducts', getProductsByUserId);
 
 module.exports = router;
