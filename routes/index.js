b09a48303fc5c3ec7d3fad90052b52ff052e0606
BrandonAllenGreen <green04@gmail.com>
2018-04-20 09:55:29 -0500

    created route for getting product type by its ID

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,4 @@ router.get('/', (req, res, next) => {
 router.use(require('./authRoute'));
 router.use(require('./productRoute'));
 
-// router.use(require('./foo'));
-
 module.exports = router;
--- routes/productRoute.js
+++ routes/productRoute.js
@@ -3,8 +3,16 @@
 const { Router } = require('express');
 const router = Router();
 
-const { getProductTypes } = require('../controllers/productCtrl.js');
+// product controller functions
+const {
+  getProductTypes,
+  getProductById
+} = require('../controllers/productCtrl.js');
 
+// routes for product types
 router.get('/prodTypes', getProductTypes);
+
+//routes for products
+router.get('/products/:id', getProductById);
 
 module.exports = router;
