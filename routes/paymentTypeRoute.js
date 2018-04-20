3cf8b838ce3bee07c6e9ada578545c194698d113
Slade Utley <slade.utley@gmail.com>
2018-04-20 13:25:53 -0500

    deleted commented out code

--- routes/paymentTypeRoute.js
+++ routes/paymentTypeRoute.js
@@ -5,7 +5,6 @@ const router = Router();
 
 const { getUserPaymentTypes } = require('../controllers/paymentTypeCtrl.js');
 
-// router.get('/currentOrder', getUserPaymentTypes);
 router.get('/currentOrder/paymentType', getUserPaymentTypes);
 
 module.exports = router;
