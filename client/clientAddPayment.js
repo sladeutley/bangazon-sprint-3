331c0b1b0f247fd2f6f13998b46cab32623ebe5c
Aaron Wolfcale <ajwolfcale@gmail.com>
2018-04-25 14:30:56 -0500

    update

--- client/clientAddPayment.js
+++ client/clientAddPayment.js
@@ -2,3 +2,12 @@ document.getElementById('addPayment').addEventListener('click', () => {
   console.log('add a new payment type');
   location.href = `${location.origin}/addPayment`;
 });
+
+
+document.getElementById('addCard').addEventListener('click', () => {
+  location.href = `${location.origin}/welcome`;
+});
+
+
+
+
