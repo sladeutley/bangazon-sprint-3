c54a26ceda719c330ea7598b14d2a70ff52e7486
Slade Utley <slade.utley@gmail.com>
2018-04-25 11:44:53 -0500

    changed add to order button to get any digit number by route params, not just one digit

--- client/orderProductClient.js
+++ client/orderProductClient.js
@@ -1,8 +1,8 @@
 //click event for adding product to current order 
 $('#addToOrder').click((e) => {
   //get orderProductId from url
-  let url = window.location.pathname;
-  let ordProdId = url.slice(-1);
+  let url = window.location.pathname.split('/');
+  let ordProdId = url[url.length - 1];
   //calls addProductToOrder function in orderCTRL
   $.ajax({
     type: "POST",
